e2b67c08dc513cbefc371cc26b78816326909156
JulianLopez2709 <judalomor27.95@gmail.com>
2024-09-12 15:16:16 -0600

    Formulario de Login creado

--- app/src/main/java/com/app/g_optics/MainActivity.kt
+++ app/src/main/java/com/app/g_optics/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.app.g_optics
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.app.g_optics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // El binding está bien declarado aquí
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+// Inflando el binding con el layoutInflater
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Configurando el root del layout en setContentView
+        setContentView(binding.root)
+
+        // Listener para el botón login
+        binding.loginButton.setOnClickListener {
+              // Verificar si el clic funciona en Logcat
+
+            // Recupera el texto de username y password
+            val username = binding.username.text.toString()
+            val password = binding.password.text.toString()
+
+            // Valida si el login es correcto o incorrecto
+            if (username == "user" && password == "1234") {
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
 }
