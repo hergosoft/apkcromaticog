eee561402aeb7c3a553bd7df129fff210f9594f3
Daniel Santiago Avila <adanielsantiago61@gmail.com>
2024-09-16 15:30:02 -0600

    Formulario de Login creado, con validacion de campos vacios

--- app/src/main/java/com/app/g_optics/MainActivity.kt
+++ app/src/main/java/com/app/g_optics/MainActivity.kt
@@ -14,27 +14,38 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-// Inflando el binding con el layoutInflater
         binding = ActivityMainBinding.inflate(layoutInflater)
-
-        // Configurando el root del layout en setContentView
         setContentView(binding.root)
 
-        // Listener para el botón login
         binding.loginButton.setOnClickListener {
-              // Verificar si el clic funciona en Logcat
+            if (checkFields()) {
+                val username = binding.username.text.toString()
+                val password = binding.password.text.toString()
 
-            // Recupera el texto de username y password
-            val username = binding.username.text.toString()
-            val password = binding.password.text.toString()
-
-            // Valida si el login es correcto o incorrecto
-            if (username == "user" && password == "1234") {
-                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+                if (username == "user" && password == "1234") {
+                    Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+                }
             }
         }
+        0
 
+    }
+
+    // Función para verificar si los campos están vacíos
+    private fun checkFields(): Boolean {
+        val username = binding.username.text.toString().trim()
+        val password = binding.password.text.toString().trim()
+
+        return if (username.isEmpty() || password.isEmpty()) {
+            // Mostrar mensaje de error si alguno de los campos está vacío
+            binding.textView5.text = "Por favor, complete todos los campos."
+            false  // Detener el flujo
+        } else {
+            // Limpiar el mensaje de error si ambos campos están completos
+            binding.textView5.text = ""
+            true  // Continuar el flujo
+        }
     }
 }
